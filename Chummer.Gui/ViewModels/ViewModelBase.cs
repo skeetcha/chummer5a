223e7af28159d7983dcbc3decbc0f11a8b1e8fc0
chummer5isalive <chummer5isalive@gmail.com>
2019-09-18 23:15:47 +1000

    - Bare minimum required to compile a hello world.

---
+++ Chummer.Gui/ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ReactiveUI;
+
+namespace Chummer.Gui.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
