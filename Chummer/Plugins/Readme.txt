23ed65ed7bda9730dd856d30be03d60594052d37
Daniel <pianojammer@comcast.net>
2020-03-02 17:40:07 -0800

    Starting over

--- Chummer/Plugins/Readme.txt
+++
@@ -1,1 +0,0 @@
-This folder holds dll's, that implement a MEF-Plugin interface and are loaded at runtime from Chummer.
--- TextblockConverter/App.xaml.cs
+++
@@ -1,9 +0,0 @@
-namespace WpfApplication1
-{
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App
-    {
-    }
-}
