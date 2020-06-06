d74e845a65e4a7cd6e51008d3583273d573095a0
Daniel Unterholzner <pianojammer@comcast.net>
2020-06-06 16:19:02 -0700

    Removing references to moved files

--- src/renderer.ts
+++
@@ -1,9 +0,0 @@
-const TabGroup = require("electron-tabs");
-
-let tabGroup = new TabGroup({});
-
-/*let tab1 = tabGroup.addTab({
-    title: "Our Code World",
-    src: "https://ourcodeworld.com",
-    visible: true
-});*/
